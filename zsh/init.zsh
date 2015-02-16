271066ca59fb7e5884362d599002bea0c616f16c
Bryan Konowitz <bryan@konowitz.me>
2015-02-16 11:12:56 -0800

    history tweaks for zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,6 +1,28 @@
-# Defines aliases and functions for a kono-style shell
+# Defines config for a kono-style zshell
 
 CUR="$(dirname $0)"
 source "$CUR/functions.zsh"
 source "$CUR/aliases.zsh"
 
+# awesome cd movements from zshkit
+setopt AUTOCD
+setopt AUTOPUSHD PUSHDMINUS PUSHDSILENT PUSHDTOHOME
+setopt cdablevars
+
+# Save a metric ton of history
+HISTSIZE=20000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history # share command history data
+
+# show contents after cd'ing
+chpwd() {
+  ls -AlhF --color=auto --group-directories-first
+}
