cdf5e54464f6334b40f7f39643cf24153e6bce3f
Bryan Konowitz <bryan@konowitz.me>
2015-04-24 23:26:39 -0700

    added docker plugins

---
+++ docker/aliases.zsh
@@ -0,0 +1,8 @@
+# Docker
+alias dps='docker ps'
+alias ds!='docker stop'
+alias ds='docker start'
+
+# Docker Compose
+alias dcp='docker-compose pull --allow-inseucre-ssl'
+alias dcu='docker-compose up'
