2fd181b62378cd460d2e742b241781436e9fdf13
Bryan Konowitz <bryan@konowitz.me>
2015-04-26 22:07:50 -0700

    missed a couple docker-compose aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,5 +4,7 @@ alias ds!='docker stop'
 alias ds='docker start'
 
 # Docker Compose
+alias dc='docker-compose'
+alias dcb='docker-compose build'
 alias dcp='docker-compose pull --allow-inseucre-ssl'
 alias dcu='docker-compose up'
