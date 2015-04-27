f54a8c088b003edabcc86f2b0b483330f17bbd01
Bryan Konowitz <bryan@konowitz.me>
2015-04-27 10:12:41 -0700

    boot2docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -8,3 +8,11 @@ alias dc='docker-compose'
 alias dcb='docker-compose build'
 alias dcp='docker-compose pull --allow-inseucre-ssl'
 alias dcu='docker-compose up'
+
+# Boot2docker
+alias bd='boot2docker'
+alias bds='boot2docker ssh'
+alias bdr='boot2docker restart'
+alias bdd='boot2docker down'
+alias bdu='boot2docker up'
+
