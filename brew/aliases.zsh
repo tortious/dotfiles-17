e563716e1179faa4eef61d29900dfd6895387b2d
Bryan Konowitz <bryan@konowitz.me>
2015-05-16 15:27:55 -0700

    adding brew aliases

---
+++ brew/aliases.zsh
@@ -0,0 +1,13 @@
+# brew
+alias bri='brew install'
+alias brs='brew search'
+alias brrm='brew rm'
+alias bru='brew update'
+
+# brew cask
+alias bci='brew cask install'
+alias bci!='brew cask install -f'
+alias bcin='brew cask info'
+alias bcs='brew cask search'
+alias bcrm='brew cask rm'
+
