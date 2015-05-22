cc137d21f2e4612c77e1177db353b6dafc163799
Bryan Konowitz <bryan@kono.sh>
2015-05-22 11:12:37 -0700

    added dlft for docker logs

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -2,6 +2,10 @@ function dclc() {
   docker ps -a | awk 'NR > 1 {print $1}' | xargs docker rm
 }
 
+function dlft() {
+  docker logs -f --tail=$1 $2
+}
+
 # function dclin() {
 #   eval "docker rmi $(docker images -a | awk '/$1/ {print $3}')"
 # }
