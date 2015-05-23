3e53b09a56e61779e236762cc40ae1879d569a2c
Bryan Konowitz <bryan@kono.sh>
2015-05-23 00:53:20 -0700

    added a func for setting up the extra args of dns on boot2docker

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -6,6 +6,12 @@ function dlft() {
   docker logs -f --tail=$1 $2
 }
 
+function b2ddns!() {
+  DOCKER_BRIDGE=`boot2docker ssh ifconfig docker0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+  boot2docker ssh sudo "ash -c \"echo EXTRA_ARGS=\'--dns $DOCKER_BRIDGE --dns $(scutil --dns | awk -F ': ' '/nameserver/{print $2}' | head -1) --dns-search service.consul\' > /var/lib/boot2docker/profile\""
+  boot2docker restart
+}
+
 # function dclin() {
 #   eval "docker rmi $(docker images -a | awk '/$1/ {print $3}')"
 # }
