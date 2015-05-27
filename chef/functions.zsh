b682cd137c38700938244c14e095c1e6a2251812
Bryan Konowitz <bryan@kono.sh>
2015-05-27 14:31:54 -0700

    added knife ssh func

--- chef/functions.zsh
+++ chef/functions.zsh
@@ -6,6 +6,7 @@ function kesdc() {
   knife ec2 server delete -N $1 --purge -y
 }
 function kesdn() { knife ec2 server delete --node-name $1 --purge }
+function kssh() { knife ssh "name:$1" "$*" -x 'ec2-user' }
 function kvcd() { 
   knife vault create $1 $2 -A bkonowitz,cschwoegler,mcowgill,sschwoegler -S "role:${1}_${2}_vault"
 }
