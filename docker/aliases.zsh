f04315ca80a4d54793c7649ee23e8e8b08338b82
Bryan Konowitz <bryan@kono.sh>
2015-09-19 20:29:32 -0700

    introducing initial docker-machine aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -12,6 +12,13 @@ alias dck='docker-compose kill'
 alias dcp='docker-compose pull --allow-insecure-ssl'
 alias dcu='docker-compose up'
 
+# Docker Machine
+alias dm='docker-machine'
+alias dmls='docker-machine ls'
+alias dmrm='docker-machine rm'
+alias dmk='docker-machine stop'
+alias dms='docker-machine start'
+
 # Boot2docker
 alias bd='boot2docker'
 alias bds='boot2docker ssh'
