af3fcdff0cdaa3be1cff0f2631548ff091fa39f8
Bryan Konowitz <bryan@kono.sh>
2016-03-27 01:26:23 -0700

    fixing paths for use_go in direnvrc and killing java 8 permsize param warnings

--- java/init.zsh
+++ java/init.zsh
@@ -5,6 +5,6 @@ source "$CUR/functions.zsh"
 source "$CUR/aliases.zsh"
 
 # go big or go home
-export JAVA_OPTS="-Xms1048M -Xmx2048M -XX:MaxPermSize=1024M"
+export JAVA_OPTS="-Xms1048M -Xmx2048M"
 export MAVEN_OPTS=${JAVA_OPTS}
 export SBT_OPTS=${JAVA_OPTS}
