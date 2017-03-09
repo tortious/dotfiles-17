f3846c96d79225531de7bca8e93ae200aed810a4
Bryan Konowitz <bryan@kono.sh>
2017-03-08 22:20:40 -0800

    added macos with pwdf and cdf functions

---
+++ macos/functions.zsh
@@ -0,0 +1,16 @@
+# # pwdf: echoes path of front-most window of Finder
+pwdf ()
+{ 
+   currFolderPath=$( /usr/bin/osascript <<"   EOT"
+       tell application "Finder"
+           try
+               set currFolder to (folder of the front window as alias)
+           on error
+               set currFolder to (path to desktop folder as alias)
+           end try
+           POSIX path of currFolder
+       end tell
+   EOT
+   )
+   echo "$currFolderPath"
+}
---
+++ macos/init.zsh
@@ -0,0 +1,5 @@
+# Defines ruby aliases, functions, and variable exports for a clean ruby env
+
+CUR="$(dirname $0)"
+source "$CUR/functions.zsh"
+source "$CUR/aliases.zsh"
